69ed13aa77aa696db295d074fe01b02a51bf54fa
Tobe O <thosakwe@gmail.com>
2018-07-29 16:53:49 -0400

    CMake boilerplate

---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+// Copyright (c) 2018, Tobechukwu Osakwe.
+//
+// All rights reserved.
+//
+// Use of this source code is governed by a
+// GPLv3-style license that can be found in the LICENSE file.
